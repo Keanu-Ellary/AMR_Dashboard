734c727193df511b094cd2d9888fe93f1fb7f473
RichterRSA <simonvdm132@gmail.com>
2026-06-18 21:35:29 +0200

    Fix the failing test

--- amr-dashboard/app/api/users/route.ts
+++ amr-dashboard/app/api/users/route.ts
@@ -1,4 +1,5 @@
 import { getUsers } from "@/functions/users/getUsers";
+import { register } from "@/functions/users/registerUser";
 
 export async function GET(req: Request) 
 {
@@ -17,4 +18,19 @@ export async function GET(req: Request)
     return Response.json(res.body, {
         status: res.statusCode,
     });
-};
+}
+
+export async function POST(req: Request) {
+    const body = await req.json();
+
+    const res = await register({
+        name: body.name,
+        surname: body.surname,
+        email: body.email,
+        password: body.password,
+    });
+
+    return Response.json(res.body, {
+        status: res.statusCode,
+    });
+}
